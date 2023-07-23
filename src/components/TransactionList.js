77b5d76e1ca32b2ade183fede58e93df9ba640c9
Moringa School <chepfaith18@gmail.com>
2023-07-23 17:56:00 +0300

    Defining the TransactionList compent

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -2,3 +2,6 @@
 import React, { useState, useEffect } from 'react'; // Importing required React hooks for functional components
 import axios from 'axios'; // Importing axios for making HTTP requests
 import './TransactionList.css'; // Importing the CSS file for styling the TransactionList component
+
+// Define the TransactionList component
+const TransactionList = () => {
