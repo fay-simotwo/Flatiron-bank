df163dfbdb96e98e69bc8733f3f677680c0a9d8c
Moringa School <chepfaith18@gmail.com>
2023-07-23 21:26:38 +0300

    Styles updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import TransactionList from './components/TransactionList';
-import { Helmet } from 'react-helmet-async';
+
 
 
 const App = () => {
   return (
     <div className="App">
-      <Helmet>
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" />
-      </Helmet>
+     
       <TransactionList />
 
     </div>
