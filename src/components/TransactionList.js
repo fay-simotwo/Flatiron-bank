b1ede5b527e500d120cf64d6afed055d68fa86e4
Moringa School <chepfaith18@gmail.com>
2023-07-23 17:53:24 +0300

    Importing necesarry modules and styles on TransactionList.js file

---
+++ src/components/TransactionList.js
@@ -0,0 +1,4 @@
+// Import necessary modules and styles
+import React, { useState, useEffect } from 'react'; // Importing required React hooks for functional components
+import axios from 'axios'; // Importing axios for making HTTP requests
+import './TransactionList.css'; // Importing the CSS file for styling the TransactionList component
